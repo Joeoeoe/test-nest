a71992eed388812a7fb305f15148d37a9c061fb1
Jonithan <695993410@qq.com>
2020-10-12 10:05:13 +0800

    init

---
+++ src/cats/cats.controller.ts
@@ -0,0 +1,43 @@
+import {
+  Controller,
+  Get,
+  Query,
+  Post,
+  Body,
+  Put,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { CreateCatDto, UpdateCatDto, ListAllEntities } from './dto/dto';
+import { CatsService } from './cats.service';
+import { Cat } from './interface/cat.interfacr';
+
+@Controller('cats')
+export class CatsController {
+  constructor(private catsService: CatsService) {}
+
+  @Post()
+  async create(@Body() createCatDto: CreateCatDto) {
+    this.catsService.create(createCatDto);
+  }
+
+  @Get()
+  async findAll(): Promise<Cat[]> {
+    return this.catsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `This action returns a #${id} cat`;
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() updateCatDto: UpdateCatDto) {
+    return `This action updates a #${id} cat`;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `This action removes a #${id} cat`;
+  }
+}
---
+++ src/cats/dto/dto.ts
@@ -0,0 +1,11 @@
+export class CreateCatDto {
+  name: string;
+  age: number;
+  breed: string;
+}
+
+export class UpdateCatDto {}
+
+export class ListAllEntities {
+  limit: number;
+}
